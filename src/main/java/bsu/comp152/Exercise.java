3b05d5fee2543da3b1084202caaf9e7e296b0900
lkgross <profgross@gmail.com>
2023-01-25 10:52:11 -0500

    Initial commit

---
+++ src/main/java/bsu/comp152/Exercise.java
@@ -0,0 +1,26 @@
+package bsu.comp152;
+
+/**
+ * InsulinDosage -
+ * A class for Project 1, COMP 152
+ *
+ * Starter code Laura K. Gross, laura.gross@bridgew.edu, 1/2023
+ *
+ * Completed by: [student name], [student email], [date of completion]
+ */
+public class Exercise {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * percentage -
+     * A static method that returns [insert description of method here]
+     * @param daysMetTarget
+     * @param daysInYear
+     * @return
+     */
+    public static double percentage(int daysMetTarget, int daysInYear) {
+        return 0.0;
+    }
+}
